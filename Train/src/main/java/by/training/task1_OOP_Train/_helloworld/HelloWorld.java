47defd77542258346776dc0b31be01ba13e99e3b
Oleg <oleg-bondarev-98@mail.ru>
2019-07-03 14:19:48 +0300

    add logger

--- Train/src/main/java/by/training/task1_OOP_Train/_helloworld/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package by.training.task1_OOP_Train._helloworld;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
---
+++ Train/src/main/java/by/training/train/_helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+package by.training.train._helloworld;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *   @author Oleg Bondarev
+ *   @version 1.0
+ */
+final class HelloWorld {
+    private HelloWorld() { }
+
+    private static final Logger LOGGER = LogManager.getLogger(HelloWorld.class);
+    /**
+     * @param args argument string parameters
+     */
+    public static void main(final String[] args) {
+        LOGGER.debug("Debug Message Logged !!!");
+        LOGGER.info("Info Message Logged !!! Hello World !!!");
+        LOGGER.error("Error Message Logged !!!", new NullPointerException("NullError"));
+    }
+}
